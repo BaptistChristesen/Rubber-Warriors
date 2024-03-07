d2c45c16b72ca3c27b9088691ebf76a17cb36a35
Josh Madison <jmad99@sbcglobal.net>
2024-03-07 09:40:08 -0600

    started data class structure for ducks

---
+++ Rubber Warriors/Ducks.swift
@@ -0,0 +1,12 @@
+//
+//  DuckGrid.swift
+//  Rubber Warriors
+//
+//  Created by Josh Madison on 3/6/24.
+//
+
+import Foundation
+
+@Observable class DuckGrid: Identifiable {
+    var isOccupied = false
+}
