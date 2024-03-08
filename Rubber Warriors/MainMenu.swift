1cc30efb9e4320f3af49d0cd54471962fa740691
Caden Christesen <cadenchristesen@gmail.com>
2024-03-08 16:17:26 -0600

    brody built some homescreen

---
+++ Rubber Warriors/MainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenu.swift
+//  Rubber Warriors
+//
+//  Created by Caden Christesen on 3/8/24.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenu()
+}
