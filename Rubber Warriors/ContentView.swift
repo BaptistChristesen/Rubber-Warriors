38bdc57cc87ee351ecf69ab7f3d1a77cab37e34c
Caden Christesen <cadenchristesen@gmail.com>
2024-03-12 21:43:48 -0500

    Fixed code configuration, created attack button and functionality, created "total" values so progress bars will go down when the ducks are attacked. This functionality does not update properly, please look into.

--- Rubber Warriors/ContentView.swift
+++ Rubber Warriors/ContentView.swift
@@ -8,162 +8,201 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var selectedDuck1: String = "Original"
-  @State private var selectedDuck2: String = "Original"
-  @State private var selectedDuck3: String = "Original"
+    @State private var selectedDuck1: String = "Original"
+    @State private var selectedDuck2: String = "Original"
+    @State private var selectedDuck3: String = "Original"
     @State var DUckSelection1 = "Original"
     @State var DUckSelection2 = "Original"
     @State var DUckSelection3 = "Original"
-  let duckTypes = ["Original", "Fire", "Water", "Earth", "Ice"]
+    @State private var attackTurn: Int = 0
+    @State private var whichDuck = Int.random(in: 0..<2)
+    let duckTypes = ["Original", "Fire", "Water", "Earth", "Ice"]
+    
 
-  // State variables for health and attack
-  @State private var hp1 = 100
-  @State private var hp2 = 100
-  @State private var hp3 = 100
-  @State private var dmg1 = 50 // Renamed dmg to dmg (damage) for clarity
-  @State private var dmg2 = 50
-  @State private var dmg3 = 50
+    // State variables for health and attack
+    @State private var hp1 = 100.00
+    @State private var hp2 = 100.00
+    @State private var hp3 = 100.00
+    @State private var hp1Total = 100.00
+    @State private var hp2Total = 100.00
+    @State private var hp3Total = 100.00
+    @State private var dmg1 = 50.00
+    @State private var dmg2 = 50.00
+    @State private var dmg3 = 50.00
+    @State private var dmg1Total = 50.00
+    @State private var dmg2Total = 50.00
+    @State private var dmg3Total = 50.00
+    @State private var enemyHp = 500.00
+    @State private var enemyDmg = 50.00
 
-  var body: some View {
-    VStack {
-  //      Image(DUckSelection1)
-
-//      HStack {
-//          DropdownMenu(title: "Duck 1", selection: selectedDuck1, options: duckTypes, duck: selectedDuck1)
-//          DropdownMenu(title: "Duck 2", selection: selectedDuck2, options: duckTypes, duck: selectedDuck2)
-//          DropdownMenu(title: "Duck 3", selection: selectedDuck3, options: duckTypes, duck: selectedDuck3)
-//          Button("fire") {
-//              DUckSelection1 = "Fire"
-//          }
-//          Button("Ice") {
-//              DUckSelection1 = "Ice"
-//          }
-//          Button("Original") {
-//              DUckSelection1 = "Original"
-//          }
-//          
-//          Menu {
-//              ForEach(duckTypes, id: \.self) { option in
-//                  Button(option) { selectedDuck1 = option
-//                      DUckSelection1 = selectedDuck1
-//                      
-//                  }
-//              }
-//          } label: {
-//              Text(selectedDuck1)
-//                  .foregroundColor(.black)
-//                  .frame(maxWidth: .infinity)
-//                  .padding(.horizontal)
-//          }
-//      }
-      ScrollView(.horizontal) {
-            //ForEach(0..<3) { index in{
-            ZStack {
-              HStack {
-                // Check the selection and display the corresponding image
-                  VStack {
-                      //Drop down start
-                      Menu {
-                          ForEach(duckTypes, id: \.self) { option in
-                              Button(option) { selectedDuck1 = option
-                                  DUckSelection1 = selectedDuck1
-                                  
-                              }
-                          }
-                      } label: {
-                          Text(selectedDuck1)
-                              .foregroundColor(.black)
-                              .frame(maxWidth: .infinity)
-                              .padding(.horizontal)
-                      }
-                      //Dropdown end
-                      Image(DUckSelection1)
-                          .resizable()
-                          .frame(width: 100, height: 100)
-                      Text("Health")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 100, total: 100)
-                      Text("Attack")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 60, total: 100)
-                  }
-                  .frame(width: 110, height: 200)
-
-                  VStack {
-                      //start
-                      Menu {
-                          ForEach(duckTypes, id: \.self) { option in
-                              Button(option) { selectedDuck2 = option
-                                  DUckSelection2 = selectedDuck2
-                                  
-                              }
-                          }
-                      } label: {
-                          Text(selectedDuck2)
-                              .foregroundColor(.black)
-                              .frame(maxWidth: .infinity)
-                              .padding(.horizontal)
-                      }
-                      //end
-                      Image(DUckSelection2)
-                          .resizable()
-                          .frame(width: 100, height: 100)
-                      Text("Health")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 100, total: 100)
-                      Text("Attack")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 60, total: 100)
-                  }
-                  .frame(width: 110, height: 200)
-
-                  ZStack {
-                      
-                  VStack {
-                      
-                          //start
-                          Menu {
-                              ForEach(duckTypes, id: \.self) { option in
-                                  Button(option) { selectedDuck3 = option
-                                      DUckSelection3 = selectedDuck3
-                                      
-                                  }
-                              }
-                          } label: {
-                              Text(selectedDuck3)
-                                  .foregroundColor(.black)
-                                  .frame(maxWidth: .infinity)
-                                  .padding(.horizontal)
-                          }
-                          .frame(width: 110)
-                          //end
-                          Image(DUckSelection3)
-                              .resizable()
-                              .frame(width: 100, height: 100)
-                      Text("Health")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 100, total: 100)
-                      Text("Attack")
-                          .font(.system(size: 12, weight: .light, design: .serif))
-                      ProgressView(value: 60, total: 100)
-                      }
-                  }
-                  .frame(width: 110, height: 200)
-//                Text("Health")
-//                  .font(.system(size: 12, weight: .light, design: .serif))
-//                ProgressView(value: Float(health(for: index)), total: 100)
-//                Text("Attack")
-//                  .font(.system(size: 12, weight: .light, design: .serif))
-//                ProgressView(value: Float(attack(for: index)), total: 100)
-              }
+    
+    var body: some View {
+        VStack {
+            Button("Attack") {
+                if(attackTurn % 2 == 0){
+                    enemyHp = enemyHp - (dmg1 + dmg2 + dmg3)
+                }
+                else{
+                    if(whichDuck == 0){
+                        hp1 = hp1 - enemyDmg
+                    }
+                    else if(whichDuck == 1){
+                        hp2 = hp2 - enemyDmg
+                    }
+                    else{
+                        hp3 = hp3 - enemyDmg
+                    }
+                }
             }
-         // }
-        }
-    }.position(CGPoint(x: 225, y: 685))
+            //      Image(DUckSelection1)
+            
+            //      HStack {
+            //          DropdownMenu(title: "Duck 1", selection: selectedDuck1, options: duckTypes, duck: selectedDuck1)
+            //          DropdownMenu(title: "Duck 2", selection: selectedDuck2, options: duckTypes, duck: selectedDuck2)
+            //          DropdownMenu(title: "Duck 3", selection: selectedDuck3, options: duckTypes, duck: selectedDuck3)
+            //          Button("fire") {
+            //              DUckSelection1 = "Fire"
+            //          }
+            //          Button("Ice") {
+            //              DUckSelection1 = "Ice"
+            //          }
+            //          Button("Original") {
+            //              DUckSelection1 = "Original"
+            //          }
+            //
+            //          Menu {
+            //              ForEach(duckTypes, id: \.self) { option in
+            //                  Button(option) { selectedDuck1 = option
+            //                      DUckSelection1 = selectedDuck1
+            //
+            //                  }
+            //              }
+            //          } label: {
+            //              Text(selectedDuck1)
+            //                  .foregroundColor(.black)
+            //                  .frame(maxWidth: .infinity)
+            //                  .padding(.horizontal)
+            //          }
+            //      }
+            
+            
+            
+            
+            
+            
+            
+            ScrollView(.horizontal) {
+                //ForEach(0..<3) { index in{
+                ZStack {
+                    HStack {
+                        // Check the selection and display the corresponding image
+                        VStack {
+                            //Drop down start
+                            Menu {
+                                ForEach(duckTypes, id: \.self) { option in
+                                    Button(option) { selectedDuck1 = option
+                                        DUckSelection1 = selectedDuck1
+                                        
+                                    }
+                                }
+                            } label: {
+                                Text(selectedDuck1)
+                                    .foregroundColor(.black)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.horizontal)
+                            }
+                            //Dropdown end
+                            Image(DUckSelection1)
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                            Text("Health")
+                                .font(.system(size: 12, weight: .light, design: .serif))
+                            ProgressView(value: hp1, total: hp1Total)
+                            
+                            
+//Whats is the attack bar for?
+//Will they gain/lose attack damage over time?
+                            Text("Attack")
+                                .font(.system(size: 12, weight: .light, design: .serif))
+                            ProgressView(value: dmg1, total: dmg1Total)
+                        }
+                        .frame(width: 110, height: 200)
+                        
+                        VStack {
+                            //start
+                            Menu {
+                                ForEach(duckTypes, id: \.self) { option in
+                                    Button(option) { selectedDuck2 = option
+                                        DUckSelection2 = selectedDuck2
+                                        
+                                    }
+                                }
+                            } label: {
+                                Text(selectedDuck2)
+                                    .foregroundColor(.black)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.horizontal)
+                            }
+                            //end
+                            Image(DUckSelection2)
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                            Text("Health")
+                                .font(.system(size: 12, weight: .light, design: .serif))
+                            ProgressView(value: hp2, total: hp2Total)
+                            Text("Attack")
+                                .font(.system(size: 12, weight: .light, design: .serif))
+                            ProgressView(value: dmg2, total: dmg2Total)
+                        }
+                        .frame(width: 110, height: 200)
+                        
+                        ZStack {
+                            
+                            VStack {
+                                
+                                //start
+                                Menu {
+                                    ForEach(duckTypes, id: \.self) { option in
+                                        Button(option) { selectedDuck3 = option
+                                            DUckSelection3 = selectedDuck3
+                                            
+                                        }
+                                    }
+                                } label: {
+                                    Text(selectedDuck3)
+                                        .foregroundColor(.black)
+                                        .frame(maxWidth: .infinity)
+                                        .padding(.horizontal)
+                                }
+                                .frame(width: 110)
+                                //end
+                                Image(DUckSelection3)
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                Text("Health")
+                                    .font(.system(size: 12, weight: .light, design: .serif))
+                                ProgressView(value: hp3, total: hp3Total)
+                                Text("Attack")
+                                    .font(.system(size: 12, weight: .light, design: .serif))
+                                ProgressView(value: dmg3, total: dmg3Total)
+                            }
+                        }
+                        .frame(width: 110, height: 200)
+                        //                Text("Health")
+                        //                  .font(.system(size: 12, weight: .light, design: .serif))
+                        //                ProgressView(value: Float(health(for: index)), total: 100)
+                        //                Text("Attack")
+                        //                  .font(.system(size: 12, weight: .light, design: .serif))
+                        //                ProgressView(value: Float(attack(for: index)), total: 100)
+                    }
+                }
+                // }
+            }
+        }.position(CGPoint(x: 225, y: 670))
     }
-  }
+}
 
-  // Helper function to get the selected duck type for a given index
+// Helper function to get the selected duck type for a given index
 //  private func selectedDuck(for index: Int) -> String {
 //    switch index {
 //    case 0:
@@ -219,31 +258,31 @@ struct ContentView: View {
 //      }
 //    }
 
+
+struct DropdownMenu: View {
     
-    struct DropdownMenu: View {
-        
-        let title: String
-        @State var selection: String
-        let options: [String]
-        @State var duck : String
-        
-        var body: some View {
-            Menu {
-                ForEach(options, id: \.self) { option in
-                    Button(option) { selection = option
-                        duck = selection
-                        
-                    }
+    let title: String
+    @State var selection: String
+    let options: [String]
+    @State var duck : String
+    
+    var body: some View {
+        Menu {
+            ForEach(options, id: \.self) { option in
+                Button(option) { selection = option
+                    duck = selection
+                    
                 }
-            } label: {
-                Text(selection)
-                    .foregroundColor(.black)
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal)
             }
+        } label: {
+            Text(selection)
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal)
         }
     }
+}
 
-    #Preview {
-      ContentView()
-    }
+#Preview {
+    ContentView()
+}
